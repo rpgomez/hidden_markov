61de91ca3737ade4ee02dd7486ad059688ef4f7f
Rodrigo P. Gomez <armyofthepenguin@gmail.com>
2021-02-05 23:12:13 -0500

    Added install_requires argument to setup.
    I found out the hard way that I need to specify the requirements for
    my package in order for it to install successfully in a vanilla python
    environment as opposed to an anaconda installation.

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import setup, find_packages
 
-setup(name="hmm",version="0.2", packages=find_packages(),
-            py_modules = ['hmm'])
+setup(name="hmm",version="0.2.1", packages=find_packages(),
+            py_modules = ['hmm'],
+      install_requires = ['numpy','scipy','tqdm','numba'])
