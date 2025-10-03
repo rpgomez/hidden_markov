e03db85746337544d51e07684e2ce93761f28d1e
Rodrigo P. Gomez <armyofthepenguin@gmail.com>
2025-10-02 20:59:05 -0400

    Removig superfluous build file.
    Switching to pyproject.toml means I no longer require a setup.py

--- setup.py
+++
@@ -1,5 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(name="hmm",version="0.2.1", packages=find_packages(),
-            py_modules = ['hmm'],
-      install_requires = ['numpy','scipy','tqdm','numba'])
